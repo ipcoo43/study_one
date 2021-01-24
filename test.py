ed3d345356346413a78acd6bad0a4da32ccf82a5
ipcoo43 <ipcoo43@gmail.com>
2021-01-24 22:28:15 +0900

    test.py

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
 import os
 os.system('clear')
 
-print('Good morning')
+fruits = ['Orange', 'Apple', 'Banana']
+fruits.extend(['grape'])
+
+i = 0
+while (i < len(fruits)):
+    print(fruits[i])
+    i += 1
+
+i, sum = 0, 0
+while (i >= 0):
+    i += 1
+    if (i > 10 and i < 20):
+        continue
+    sum += i
+    if (i == 30):
+        print('end!!', sum)
+        break
